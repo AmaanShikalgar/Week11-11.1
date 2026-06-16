33beb6fdcd9e466ad991fcd43561b6aac0eac71d
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-16 11:37:05 +0530

    added usePrev hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import React, {useEffect,useState} from "react"
-import { useFetch} from "./hooks/useFetch"
+import React, {useState} from "react";
+import { usePrev } from "./hooks/use-prev";
 
-function App (){
 
-  const[currentPost,setCurrentPost] = useState(1);
-  const {finalData} = useFetch("https://jsonplaceholder.typicode.com/todos/"+ currentPost);
+function App(){
+  const[state,setState] =useState(0);
+  const prev = usePrev(state);
 
-  return (
-    <div>
-      <button onClick={()=>setCurrentPost(1)}>1</button>
-      <button onClick={()=>setCurrentPost(2)}>2</button>
-      <button onClick={()=>setCurrentPost(3)}>3</button>
-      {JSON.stringify(finalData)}
-    </div>
+  return(
+    <>
+    <p>{state}</p>
+    <button onClick={()=>setState(curr=>curr+1)}>Click ME</button>
+    <p>THe previous value was {prev} </p>
+    </>
   )
 }
 
-export default App
+export default App;
+
+
+
+
+// function App (){
+
+//   const[currentPost,setCurrentPost] = useState(1);
+//   const {finalData,loading} = useFetch("https://jsonplaceholder.typicode.com/todos/"+ currentPost);
+
+//   if(loading){
+//     return<div>
+//       Loading........
+//     </div>
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={()=>setCurrentPost(1)}>1</button>
+//       <button onClick={()=>setCurrentPost(2)}>2</button>
+//       <button onClick={()=>setCurrentPost(3)}>3</button>
+//       {JSON.stringify(finalData)}
+//     </div>
+//   )
+// }
+
+// export default App
 
 // function useCounter(){ //custom hooks
 //   const[count,setCount] = useState(0)
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,18 +2,26 @@ import React, {useEffect,useState} from "react"
 
 export function useFetch(url){
     const [finalData,setFinalData] = useState({})
+    const [loading,setLoading] =useState(true);
 
     const getDetails = async() =>{
+        setLoading(true);
         const response = await fetch(url);
         const json = await response.json();
         setFinalData(json) ;
+        setLoading(false);
     }
 
     useEffect(()=>{
         getDetails();
     },[url])
 
+    useEffect(()=>{
+        setInterval(getDetails,10*1000)
+    },[])
+    
     return {
-        finalData
+        finalData,
+        loading
     }
 }
